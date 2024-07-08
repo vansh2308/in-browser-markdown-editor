16252dacdc7ce039e53a55a4f98a7b1a00057e1a
VANSH AGARWAL <vanshagarwal11@gmail.com>
2024-07-09 00:25:31 +0530

    Make static editor

---
+++ src/components/Editor.jsx
@@ -0,0 +1,19 @@
+
+
+import data from './../assets/data.json'
+
+
+export default function Editor() {
+
+    return (
+        <div className="w-1/2 h-full flex flex-col gap-2">
+            <p className=" tracking-[0.2rem] text-xs"> EDITOR </p>
+            <textarea
+                className="w-full overflow-auto p-6 bg-dsecondary rounded-md text-sm flex-grow focus:outline-none"
+                value={ data[0].content }
+                onChange={(e) => e.preventDefault()}
+            />
+
+        </div>
+    )
+}
